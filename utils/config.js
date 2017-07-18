fe418fea8a0859e7c8394ebd27d793438ab68eb9
benediktbosshammer <benedikt.bosshammer@sap.com>
2017-07-18 13:43:59 +0000

    HCP Deploy Adaptions: templateUrl MUST be with .html file extension to enable the hcp to open it

--- utils/config.js
+++ utils/config.js
@@ -9,13 +9,13 @@ app.config(function($stateProvider, $mdThemingProvider) {
     .state('locations-create-information', {templateUrl: 'pages/locations-create-information.html'})
     .state('locations-manual-adress', {templateUrl: 'pages/manual-adress-form.html'})
 
-    .state('locations-water-decision', {templateUrl: 'pages/locations-water-decision'})
-    .state('locations-water-decision-interest', {templateUrl: 'pages/locations-water-decision-interest'})
-    .state('locations-water-decision-no-interest', {templateUrl: 'pages/locations-water-decision-no-interest'})
+    .state('locations-water-decision', {templateUrl: 'pages/locations-water-decision.html'})
+    .state('locations-water-decision-interest', {templateUrl: 'pages/locations-water-decision-interest.html'})
+    .state('locations-water-decision-no-interest', {templateUrl: 'pages/locations-water-decision-no-interest.html'})
 
-    .state('locations-toilet-paper-decision', {templateUrl: 'pages/locations-toilet-paper-decision'})
-    .state('locations-toilet-paper-decision-interest', {templateUrl: 'pages/locations-toilet-paper-decision-interest'})
-    .state('locations-toilet-paper-decision-no-interest', {templateUrl: 'pages/locations-toilet-paper-decision-no-interest'})
+    .state('locations-toilet-paper-decision', {templateUrl: 'pages/locations-toilet-paper-decision.html'})
+    .state('locations-toilet-paper-decision-interest', {templateUrl: 'pages/locations-toilet-paper-decision-interest.html'})
+    .state('locations-toilet-paper-decision-no-interest', {templateUrl: 'pages/locations-toilet-paper-decision-no-interest.html'})
 
-    .state('locations-create-summary', {templateUrl: 'pages/locations-create-summary'});
+    .state('locations-create-summary', {templateUrl: 'pages/locations-create-summary.html'});
 });
