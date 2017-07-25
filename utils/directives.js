89ee3943b2f28e0badbf0a000f8aaf01e83a9b60
Benedikt Bosshammer <bene@Benedikts-MacBook-Pro.local>
2017-07-25 11:00:52 +0200

    communication history preparation

---
+++ utils/directives.js
@@ -0,0 +1,71 @@
+app.directive('sideBar', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'directives/sidebar-component.html'
+  }
+});
+
+app.directive('personInfoForm', function() {
+  return {
+    restrict: 'AE',
+    scope: {
+      nameModel: '=',
+      vornameModel: '=',
+      streetModel: '=',
+      postcodeModel: '=',
+      cityModel: '=',
+      emailModel: '=',
+      telefonModel: '=',
+      stpauliModel: '='
+    },
+    templateUrl: 'directives/person-info-component.html'
+  }
+
+});
+
+
+app.directive('locationInfoForm', function() {
+  return {
+    restrict: 'AE',
+    scope: {
+      streetModel: '=',
+      postcodeModel: '=',
+      additionaladdressModel: '=',
+      cityModel: '=',
+      formName: '='
+    },
+    templateUrl: 'directives/location-info-component.html'
+  }
+
+});
+
+app.directive('supportComponent', function() {
+  return {
+    restrict: 'AE',
+    scope: {
+      interestReasons: '=',
+      noInterestReasons: '=',
+      decisionValueModel: '='
+    },
+    templateUrl: 'directives/support-component.html'
+  }
+});
+
+app.directive('communicationHistory', function() {
+  return {
+    restrict: 'AE',
+    scope: {
+    },
+    templateUrl: 'directives/communication-history-component.html'
+  };
+});
+
+app.directive('noteList', function() {
+  return {
+    restrict: 'AE',
+    scope: {
+      notes: '='
+    },
+    templateUrl: 'directives/note-list-component.html'
+  };
+});
