3e2b37740dd556b1d105e1dcb9c0758f200a59f3
Benedikt Bosshammer <bene@Benedikts-MacBook-Pro.local>
2017-07-31 10:23:57 +0200

    Added VcA like font + moved home to pages folder

--- utils/config.js
+++ utils/config.js
@@ -2,23 +2,23 @@ app.config(function($stateProvider, $mdThemingProvider, uiGmapGoogleMapApiProvid
 uiGmapGoogleMapApiProvider.configure({
         key: 'AIzaSyAzzmCZNBcEjUJjWhaUl-PAZK7lcJs9HxE'
     });
-    
+
     	$mdThemingProvider.theme('default')
 		.primaryPalette('blue', {
     		'default': '500' // by default use shade 400 from the pink palette for primary intentions
     	//	'hue-1': '900' // use shade 100 for the <code>md-hue-1</code> class
     })
-		
+
 		.accentPalette('yellow', {
 			'default': 'A700'
 			})
 		.warnPalette('red', {
 			'default': 'A700'
 		});
-    
+
 
   $stateProvider
-    .state('home', {templateUrl: 'home.html'})
+    .state('home', {templateUrl: 'pages/home.html'})
     .state('presentation', {templateUrl: 'pages/presentation.html'})
     .state('locations', {templateUrl: 'pages/locations.html'})
     .state('contacts', {templateUrl: 'pages/contacts/contacts.html'})
