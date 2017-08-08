6cd29d930131d14f5613e7fe7a2f758e8f30e7eb
Laima Laka <laima.laka@sap.com>
2017-08-08 10:44:25 +0000

    Fix

--- utils/homepageMaps.js
+++ utils/homepageMaps.js
@@ -4,30 +4,6 @@ app.controller('HomeLocationCtrl', ['$geolocation', '$scope', function($geolocat
 // https://github.com/ninjatronic/ngGeolocation
 
 	$geolocation.getCurrentPosition({
-<<<<<<< HEAD
-            timeout: 60000,
-            maximumAge: 250,
-            enableHighAccuracy: true
-        }).then(function(position) {
-    		$scope.myPosition = position;
-    		if ($scope.myPosition) {
-	    		$scope.map = {
-				  center: {
-				    latitude: $scope.myPosition.coords.latitude,
-				    longitude: $scope.myPosition.coords.longitude
-				  },
-				  zoom: 13
-				}; 
-    		}
-         });
-     	$scope.map = {
-		  center: {
-		    latitude: 51.312801,
-		    longitude: 9.481544
-		  },
-		  zoom: 5
-		}; 
-=======
     timeout: 60000,
     maximumAge: 250,
     enableHighAccuracy: true
@@ -52,17 +28,16 @@ app.controller('HomeLocationCtrl', ['$geolocation', '$scope', function($geolocat
 	  zoom: 5
 	};
 
->>>>>>> f5ab20a483c4aa8784842dc94eaa6dab9746a8b7
  // $scope.map = { center: { latitude: 53.563384, longitude: 9.991794 }, zoom: 15 }; location from Hamburg
   $scope.options = {scrollwheel: false};
 
   var _options = {
     icon: {
         url: '/sources/img/icons/dropgmarkerblue.png',
-        scaledSize: new google.maps.Size(42,68)
+        scaledSize: new google.maps.Size(42, 68)
     }
   };
-// https://www.iflebenskunde.de/wp-content/uploads/2016/07/Viva-con-Agua.png 
+
   $scope.markers = [{
     id: 0,
     coords: {
@@ -77,8 +52,4 @@ app.controller('HomeLocationCtrl', ['$geolocation', '$scope', function($geolocat
       longitude: 9.991254 },
     options: _options
   }];
-<<<<<<< HEAD
 }]);
-=======
-}]);
->>>>>>> f5ab20a483c4aa8784842dc94eaa6dab9746a8b7
