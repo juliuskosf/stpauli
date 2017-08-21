f1ff01157b867cd9ff49328871b7136b37cdf1ed
Laima Laka <laima.laka@sap.com>
2017-08-21 15:28:44 +0000

    Progress Bar

---
+++ utils/progressBar.js
@@ -0,0 +1,17 @@
+app.config(function($mdThemingProvider) {
+  })
+  app.controller('AppCtrl', ['$scope', '$interval', function($scope, $interval) {
+    var self = this, j= 0, counter = 0;
+
+    self.mode = 'query';
+    self.activated = true;
+    self.determinateValue1 = 25;
+    self.determinateValue2 = 50;
+    self.determinateValue3 = 75;
+    self.determinateValue4 = 100;
+    
+
+    self.showList = [ ];
+
+ 
+  }]);
