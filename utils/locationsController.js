0388865162d32c61b2fe0c8fccfb4dc512410a3c
benediktbosshammer <bosshammer.benedikt@student.dhbw-karlsruhe.de>
2017-08-29 08:11:15 +0000

    small bug fixes

--- utils/locationsController.js
+++ utils/locationsController.js
@@ -430,10 +430,6 @@ app.controller('locationsDetailCtrl', ['$rootScope', '$scope', '$state', '$mdDia
 		$scope.backClicked = function() {
 			$state.go($state.previous);
 		};
-		
-		$scope.goBack = function (){
-    		$state.go($state.previous);
-    	};
 
 		$scope.interestReasons = locationService.getInterestReasons();
 
